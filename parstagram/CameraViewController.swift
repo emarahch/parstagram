fc5a75f205e37aa691f1f4501198041a50dd5b76
emarahch <emarah.charles19@bcmail.cuny.edu>
2022-03-26 13:14:56 -0400

    assignment done

---
+++ parstagram/CameraViewController.swift
@@ -0,0 +1,84 @@
+//
+//  CameraViewController.swift
+//  parstagram
+//
+//  Created by Emarah Charles on 3/26/22.
+//
+
+import UIKit
+import AlamofireImage
+import Parse
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var commentField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //set up schema in parse
+    @IBAction func onSubmitButton(_ sender: Any) {
+        let  post = PFObject(className: "Posts")
+        post["caption"] = commentField.text
+        post["author"] = PFUser.current
+        
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(data: imageData!)
+        
+        post["image"] = file
+        
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("saved")
+            }else {
+                print("error!")
+                    
+                }
+            }
+        }
+    
+    
+
+    @IBAction func onCameraButton(_ sender: Any) {
+        //this is the simple way of doing it, there a more customizable ways of doing this
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera  // if on real phone use camera
+        }else { // in simulator use photolibray
+            picker.sourceType = .photoLibrary
+        }
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageScaled(to: size)
+        
+        imageView.image = scaledImage
+        
+        dismiss(animated : true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
